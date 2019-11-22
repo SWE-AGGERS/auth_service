1f0821a47d158aca641fe4b111d078c5603b88ce
selman <selman.alp@hotmail.com.tr>
2019-11-22 17:32:07 +0100

    followed and auth service merged and fixed some bug

--- auth_service/views/follow.py
+++ auth_service/views/follow.py
@@ -1,31 +1,30 @@
 from flask import Blueprint, request, jsonify
 from auth_service.database import db, Followers, User
-from flask_login import current_user, login_required
 from sqlalchemy import and_
 
 follow = Blueprint('follow', __name__)
 
 # Follow a writer
-@follow.route('/follow/<int:userid>', methods=['POST'])
-@login_required
-def follow_user(userid):
+@follow.route('/follow/<int:follower_id>/<int:followed_id>', methods=['POST'])
+
+def follow_user(follower_id,followed_id):
     # get the user who want following userid
-    subject = current_user.id
+    subject = follower_id
 
     # if try to follow himeself
-    if int(userid) == int(subject):
+    if int(followed_id) == int(subject):
         return {"followed": -2, "message": "You can't self-follow"}
 
     # if already followed
-    if is_follower(subject, userid):
+    if is_follower(subject, followed_id):
         return {"followed": -1, "message": "You already follow this user"}
 
     # if the followed user do not exist
-    if User.query.filter_by(id=userid).first() == None:
+    if User.query.filter_by(id=followed_id).first() == None:
         return {"followed": -3, "message": "The user does not exist"}
 
     # add to follower_table the tuple (follower_id, followed_id)
-    result = add_follow(subject, userid)
+    result = add_follow(subject, followed_id)
     if result == -1:
         # db.session.add error
         return {"followed": -4, "message": "DB error during add_follow"}
@@ -35,43 +34,40 @@ def follow_user(userid):
 
 
 # Unfollow a writer
-@follow.route('/follow/<int:userid>', methods=['DELETE'])
-@login_required
-def unfollow_user(userid):
+@follow.route('/follow/<int:follower_id>/<int:followed_id>', methods=['DELETE'])
+def unfollow_user(follower_id,followed_id):
     # get the user who want to unfollow userid
-    subject = current_user.id
+    subject = follower_id
 
-    if userid == subject:
+    if followed_id == subject:
         return {"followed": -2, "message": "You can't self-unfollow"}
 
     # if the followed user do not exist
-    if User.query.filter_by(id=userid).first() == None:
+    if User.query.filter_by(id=followed_id).first() == None:
         return {"followed": -3, "message": "The user does not exist"}
 
     # if user not followed
-    if not is_follower(subject, userid):
+    if not is_follower(subject, followed_id):
         return {"followed": -1, "message": "You do not already follow this user"}
 
     # remove from follower_table the tuple (follower_id, followed_id)
-    if delete_follow(subject, userid) == -1:
+    if delete_follow(subject, followed_id) == -1:
         # db delete error
-        rereturn {"followed": -4, "message": "DB error during add_follow"}
+        return {"followed": -4, "message": "DB error during add_follow"}
 
     # return OK
     return {"followed": get_followed_number(subject)}
 
 
-@follow.route('/unfollow/<int:userid>', methods=['POST'])
-@login_required
-def unfollow_user_post(userid):
+@follow.route('/unfollow//<int:follower_id>/<int:followed_id>', methods=['POST'])
+def unfollow_user_post(follower_id,followed_id):
     # Unfollow user API as POST to be compatible with forms
-    return unfollow_user(userid)
+    return unfollow_user(follower_id,followed_id)
 
 
 # TODO: add to the API doc
 # return the followers list
 @follow.route('/followers/list/<int:subject>', methods=['GET'])
-@login_required
 def followers_list(subject):
     temp = db.session.query(Followers, User).filter(
         Followers.follower_id == User.id).filter_by(followed_id=subject).all()
@@ -84,13 +80,7 @@ def followers_list(subject):
 
     return jsonify({"followers": followers})
 
-@follow.route('/followers/list', methods=['GET'])
-@login_required
-def followers_list_cu():
-    return followed_list(current_user.id)
-
 @follow.route('/followed/list/<int:subject>', methods=['GET'])
-@login_required
 def followed_list(subject):
     temp = db.session.query(Followers, User).filter(
         Followers.followed_id == User.id).filter_by(follower_id=subject).all()
@@ -103,36 +93,19 @@ def followed_list(subject):
 
     return jsonify({"followed": followed})
 
-# TODO: add to the API doc
-# Return the followed list
-@follow.route('/followed/list', methods=['GET'])
-@login_required
-def followed_list_cu():
-    return followed_list(current_user.id)
 
 # TODO: add to API doc
 # return number of followers
 @follow.route('/followers/<int:userid>', methods=['GET'])
-@login_required
 def followers_numer(userid):
     return jsonify({"followers": get_followers_number(userid)})
 
-@follow.route('/followers', methods=['GET'])
-@login_required
-def followers_numer_cu():
-    return followers_numer(currentu_ser.id)
 
 # TODO: add to API doc
 # return number of followers
 @follow.route('/followed/<int:userid>', methods=['GET'])
-@login_required
 def followed_numer(userid):
     return jsonify({"followeds": get_followed_number(userid)})
-
-@follow.route('/followed', methods=['GET'])
-@login_required
-def followed_numer_cu():
-    return followed_numer(current_user.id)
 
 
 @follow.route('/is_follower/<user_a>/<user_b>', methods=['GET'])
