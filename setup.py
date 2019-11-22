a3d074cd6d70ce5b0a18af20834b6f340b6f73a9
selman <selman.alp@hotmail.com.tr>
2019-11-22 09:22:24 +0100

    Update project structure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-setup(name='storytellers-monolith',
+setup(name='auth_service',
       version="0.1",
       packages=find_packages(),
       include_package_data=True,
